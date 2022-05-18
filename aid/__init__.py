4503ba4100c9c44daaa9090af77546a4ac03c582
franceme <9311496+franceme@users.noreply.github.com>
2022-05-18 12:39:20 -0400

    Update

--- aid/__init__.py
+++ aid/__init__.py
@@ -1,22 +1,11 @@
 from __future__ import print_function
-import os, sys, pwd, json, asana, datetime
+import os, sys, pwd, json, asana, datetime,time
 from datetime import date, timedelta
 from six import print_
 import funbelts as ut
 
-def user_select_option(message, options, chosen_option:int = None):
-    if chosen_option is None:
-        option_lst = list(options)
-        print_(message)
-        for i, val in enumerate(option_lst):
-            print_(i, ': ' + val['name'])
-        index = int(input("Enter choice (default 0): ") or 0)
-    else:
-        index = chosen_option
-    return option_lst[index]
-
 class masana(object):
-    def __init__(self,access_token:str=None,project_choice:str=None):
+    def __init__(self,access_token:str=None,project_choice:str=None, workspace_choice:str="Personal"):
         self.client = asana.Client.access_token(access_token)
 
         self.current_project = None
@@ -29,13 +18,17 @@ class masana(object):
         self.user = self.current_user['gid']
         
         self.added_tasks = {}
+        self._tags = []
+        
+        #https://developers.asana.com/docs/custom-fields
+        #self._priority = []
         if project_choice:
-            self.current_workspace = list(self.client.workspaces.find_all())[0]
+            self.current_workspace = [x for x in list(self.client.workspaces.find_all()) if x['name'] == workspace_choice][0]
             self.workspace = self.current_workspace['gid']
             
-            self.current_project = list(self.client.projects.find_all({
+            self.current_project = [x for x in list(self.client.projects.find_all({
                 'workspace':self.workspace
-            }))[0]
+            })) if x['name'] == project_choice][0]
             self.project = self.current_project['gid']
 
     def pick_workspace(self, choice:int):
@@ -44,6 +37,28 @@ class masana(object):
         return self.current_workspace
     def default_workspace(self):
         return self.pick_workspace(0)
+
+    """
+    @property
+    def priorities(self):
+        if self._priority == []:
+            #https://developers.asana.com/docs/get-a-workspaces-custom-fields
+            self._priority = [x for x in list(self.client.custom_fields.get_custom_fields_for_workspace(self.workspace)) if x['name'] == 'Priority']['enum_options']
+        return self._priority
+    """
+
+    @property
+    def tags(self):
+        if self._tags == []:
+            self._tags = list(self.client.tags.get_tags_for_workspace(self.workspace))
+        return self._tags
+
+    def add_tag(self, string_name):
+        tag = self.client.tags.create_tag({
+            'name':string_name
+        })
+        self._tags += [tag]
+        return tag
 
     def pick_project_string(self,choice:str):
         #https://developers.asana.com/docs/get-multiple-projects
@@ -82,29 +97,72 @@ class masana(object):
             return []
         return list(self.client.tasks.get_tasks_for_project(self.project))
 
-    def add_task(self, name:str, notes:str=None, due_day:str=None, due_time:str=None, sub_task_from:int=None):
+    def add_tags_to_task(self,taskid,tags=[]):
+        """
+        for tag in tags:
+            try:
+                #https://developers.asana.com/docs/get-tags-in-a-workspace
+                #Identifying Tags
+                current_tags = list(self.client.tags.get_tags_for_workspace(self.workspace))
+                searched_tag = [x for x in current_tags if x['name'] == tag]
+                if len(searched_tag) > 0:
+                    found_tag = searched_tag[0]
+                else: #https://developers.asana.com/docs/create-a-tag
+                    found_tag = self.client.tags.create_tag({
+                        'name':tag
+                    })
+                #https://developers.asana.com/docs/add-a-tag-to-a-task
+                self.client.tasks.add_tag_for_task(
+                    taskid,
+                    {
+                        'tag':found_tag['gid']
+                    }
+                )
+            except Exception as e:
+                print(f"!!Exception {e}")
+                pass
+        """
+        for tag in tags:
+            try:
+                searched_tag = [x for x in self.tags if x['name'] == tag]
+                if len(searched_tag) > 0:
+                    found_tag = searched_tag[0]
+                else: #https://developers.asana.com/docs/create-a-tag
+                    found_tag = self.add_tag(tag)
+                self.client.tasks.add_tag_for_task(
+                    taskid,
+                    {
+                        'tag':found_tag['gid']
+                    }
+                )
+            except Exception as e:
+                print(f"!!Exception {e}")
+                pass
+    def add_task(self, name:str, notes:str=None, due_day:str=None, sub_task_from:int=None, tags=[]):
         if self.current_workspace == None or self.current_project == None:
             return None
         
-        if due_day is not None or due_time is not None:
+        if due_day is not None:
             current_date = str(datetime.datetime.utcnow().isoformat()).split('T')[0]
             due_day = due_day or current_date
 
-            if due_time is not None:
-                #https://stackoverflow.com/questions/12691081/from-a-timezone-and-a-utc-time-get-the-difference-in-seconds-vs-local-time-at-t
-                local = datetime.datetime.now()
-                utc = datetime.datetime.utcnow()
-                diff = int((local - utc).days * 86400 + round((local - utc).seconds, -1))
-                hours = datetime.timedelta(seconds=diff).hour
+            if False:
+                if due_time is not None:
+                    #https://stackoverflow.com/questions/12691081/from-a-timezone-and-a-utc-time-get-the-difference-in-seconds-vs-local-time-at-t
+                    local = datetime.datetime.now()
+                    utc = datetime.datetime.utcnow()
+                    diff = int((local - utc).days * 86400 + round((local - utc).seconds, -1))
+                    hours = datetime.timedelta(seconds=diff)
+                    hours, _ = divmod(hours.seconds, 3600)
 
-                due_time = f"{due_time.hour + hours}:{due_time.minutes}:{due_time.seconds}.000"
-            else:
-                due_time = "22:00:00.000"
+                    due_time = f"{due_time.hour + hours}:{due_time.minute}:{due_time.second}.000"
+                else:
+                    due_time = "22:00:00.000"
 
-            due_date = f"{due_day}T{due_time}Z"
+            #http://strftime.net/
+            due_date = f"{due_day.strftime('%Y-%m-%dT%H:%M:%SZ')}"
         else:
             due_date = None
-        print(due_date)
         
         #Examples
         #https://github.com/Asana/python-asana/tree/master/examples
@@ -115,20 +173,20 @@ class masana(object):
                 #https://developers.asana.com/docs/create-a-tag
                 self.client.tags.create_tag(self.workspace, tag)
 
-        if sub_task_from:
+        if sub_task_from is not None:
             #https://developers.asana.com/docs/create-a-subtask
             try:
                 task_id = self.client.tasks.create_subtask_for_task(sub_task_from,{
                     'name': name,
                     'assignee':self.user,
                     'approval_status': 'pending',
-                    #'tags':tags,
                     'notes':notes,
                     'workspace':self.workspace,
                     'projects': [self.project],
                     'due_at':due_date
                 }, opt_fields=['gid'])
                 task = self.client.tasks.get_task(task_id['gid'])
+                self.add_tags_to_task(task_id['gid'], tags)
             except Exception as e:
                 print(f"!Exception {e}")
                 pass
@@ -175,7 +233,6 @@ class masana(object):
                 self.client.tasks.update_task(task_id,
                     {
                         'approval_status': 'pending',
-                        #'tags':tags,
                         'notes':notes,
                         'workspace':self.workspace,
                     })
@@ -183,18 +240,57 @@ class masana(object):
                 print(f"$Exception {e}")
                 pass
         
+            try:
+                self.add_tags_to_task(task_id, tags)
+            except Exception as e:
+                print(f"%>Exception {e}")
+                pass
+        
         if task is not None:
             self.added_tasks[task['gid']] = task
 
         return task
-    
-    def add_task_nextdays(self, name:str, notes:str=None, in_x_days:int=None, due_time:str=None, sub_task_from:int=None):
+    def add_task_nextdays(self, name:str, notes:str=None, in_x_days:int=None, due_day:datetime=None, sub_task_from:int=None, tags=[]):
         current_day = datetime.datetime.utcnow()
-        set_day = current_day + datetime.timedelta(days=in_x_days)
+        if due_day is None:
+            due_day = current_day
+        
+        nice_day = due_day.replace(day=current_day + datetime.timedelta(days=in_x_days))
 
-        year = set_day.year if set_day.year > 10 else f"0{set_day.year}"
-        month = set_day.month if set_day.month > 10 else f"0{set_day.month}" 
-        day = set_day.day if set_day.day > 10 else f"0{set_day.day}" 
-        nice_day = f"{year}-{month}-{day}"
+        return self.add_task(name=name, notes=notes, due_day=nice_day,sub_task_from=sub_task_from, tags=tags)
+    def add_reoccuring_task(self, name:str, notes:str=None, for_x_days:int=None, until:str=None, due_date:datetime=None, sub_task_from:int=None, tags=[], hour:int=None,minute:int=0,second:int=0):
+        output = []
 
-        return self.add_task(name=name, notes=notes, due_day=nice_day,due_time=due_time,sub_task_from=sub_task_from)
+        if due_date is None:
+            sdate = datetime.datetime.utcnow()
+        else:
+            sdate = due_date
+        
+        #TimeReplace https://stackoverflow.com/questions/12468823/python-datetime-setting-fixed-hour-and-minute-after-using-strptime-to-get-day
+        if hour is not None:
+            local = datetime.datetime.now()
+            utc = datetime.datetime.utcnow()
+            diff = int((local - utc).days * 86400 + round((local - utc).seconds, -1))
+            sdate=sdate.replace(hour=hour+diff+4)
+        if minute is not None:
+            sdate=sdate.replace(minute=minute)
+        if second is not None:
+            sdate=sdate.replace(second=second)
+
+        if for_x_days is not None:
+            edate = sdate + datetime.timedelta(days=for_x_days+1)
+        else:
+            edate = until + datetime.timedelta(days=2)
+
+        range_of_days = [sdate+timedelta(days=x) for x in range((edate-sdate).days)]
+        for day in range_of_days:
+            if True:
+                output += [
+                    self.add_task(name=name, notes=notes, due_day=day,sub_task_from=sub_task_from, tags=tags)
+                ]
+                waiting = 5
+                print(f"Waiting for {waiting} seconds")
+                time.sleep(waiting)
+            else:
+                print(day)
+        return output
