93cca9b050f1cd9bfb8fb1d9d18d6ec9266a91e8
franceme <9311496+franceme@users.noreply.github.com>
2022-05-18 20:34:59 -0400

    Update

--- hasana/__init__.py
+++ hasana/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import os, sys, pwd, json, asana, datetime,time
-from datetime import date, timedelta, strptime
+from datetime import date, timedelta
+from datetime import datetime as sub
 from six import print_
 import funbelts as ut
 
@@ -223,7 +224,7 @@ class masana(object):
                     'notes':notes,
                     'workspace':self.workspace,
                     'projects': [x['gid'] for x in parent_task['projects']],
-                    'due_at':strptime(parent_task['due_at'], '%Y-%m-%dT%H:%M:%SZ')
+                    'due_at':sub.strptime(parent_task['due_at'], '%Y-%m-%dT%H:%M:%SZ')
                 }, opt_fields=['gid'])
                 task = self.client.tasks.get_task(task_id['gid'])
                 self.add_tags_to_task(task_id['gid'], tags)
